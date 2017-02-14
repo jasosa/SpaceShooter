987fa20989bcc0b0f14cceb591db96c1c963de0a
Juan Antonio Sosa <juanan.sosa@gmail.com>
2017-02-14 13:26:49 +0100

    adding player movement and limits

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Helpers
+{
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax, zMin, zMax;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Assets.Scripts.Helpers;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var rigidbody = GetComponent<Rigidbody>();
+
+        setVelocity(moveHorizontal, moveVertical, rigidbody);
+        setPosition(rigidbody);
+        setRotation(rigidbody);
+    }
+
+    private void setRotation(Rigidbody rigidbody)
+    {
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    }
+
+    private void setPosition(Rigidbody rigidbody)
+    {
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax)
+            , 0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+    }
+
+    private void setVelocity(float moveHorizontal, float moveVertical, Rigidbody rigidbody)
+    {
+        rigidbody.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+    }
+}
