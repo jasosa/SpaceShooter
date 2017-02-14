bfd7361608b9a34f394a0169236dac450d2542ea
Juan Antonio Sosa <juanan.sosa@gmail.com>
2017-02-14 14:47:00 +0100

    Added shoots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+	
+	void Start () {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+	}
+}
