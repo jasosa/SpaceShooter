4f635ec065d7e57d40009e27b62ab89d2c623b36
Juan Antonio Sosa <juanan.sosa@gmail.com>
2017-02-15 21:46:59 +0100

    Added swamp of asteroids, scripts refactored

---
+++ Assets/Scripts/FormwardRandomSpeedMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormwardRandomSpeedMover : MonoBehaviour {
+
+    public float maxSpeed;
+    public float minSpeed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * Random.Range(minSpeed, maxSpeed);
+    }   
+}
