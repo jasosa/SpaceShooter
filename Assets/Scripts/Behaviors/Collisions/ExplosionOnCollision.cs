3c6767479af5be21b6120efd2dbf3668ea6dbe71
Juan Antonio Sosa <juanantonio@typeform.com>
2017-02-17 14:32:30 +0100

    Organizing Scripts

---
+++ Assets/Scripts/Behaviors/Collisions/ExplosionOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionOnCollision : MonoBehaviour {
+
+    public GameObject explosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Boundary")
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+    }
+}
