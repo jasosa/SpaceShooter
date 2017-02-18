6caa555607151abad3c2098321f9ca65032473b2
Juan Antonio Sosa <juanan.sosa@gmail.com>
2017-02-18 22:51:57 +0100

    scrolling background

---
+++ Assets/Scripts/Behaviors/General/BackgroundScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    [SerializeField]
+    private float scrollSpeed;
+    [SerializeField]
+    private float tiledSizeZ;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tiledSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
